f683d5e172832ff5dc3850f8fefa1c65a7edf32f
Chris Church <cmchurch@berkeley.edu>
2013-08-02 22:31:38 -0700

    NLTK implementation of natural language processing (proof of concept and benchmarks

--- multiprocess-pool-nltk.py
+++ multiprocess-pool-nltk.py
@@ -6,8 +6,10 @@
 #it makes all the tokens in the Moby Dick text uppercase
 #you can change the number of processes in the Pool by changing the variable chunks
 #  n.b. 
-#    with multiple cores / logical processors, you'll see an increase as you up the chunks (to your number of cores)
+#    with multiple cores / logical processors, you'll see an increase in performance as you up the chunks (to your number of cores), afterward it'll decrease in performance
+#      -- on my QuadCore, the program took 0.81s with one process; 0.58s with two process; 0.38s with four processes; and 0.4s with eight processes
 #    however, with a single core, you'll see a decrease in performance with more chunks
+#      -- on a single CPU, it took .97s with one process; 1.2s with four processes
 
 def chunker(tokens, chunks):
     '''this function chunks a set of tokens based on a number of chunks; each "chunk" of tokens will be sent to a separate process'''
@@ -23,18 +25,8 @@ def worker(tokens):
         uppercase.append(token.upper())
     return uppercase #return the chunk of tokens
 
-if __name__ == '__main__':
-    '''this is the main function that will set up our pool of processes -- the { if __name ___ '__main__': } is need in order for multiprocessing to work on Windows OS'''
-    
-    #imports were put here because they were loading twice under Windows -- this is not an issue in Linux
-    import multiprocessing #import the multiprocessing library
-    import nltk #import the nltk library
-    from nltk.book import text1 #import Moby Dick as an nltk text
-    import operator #import the operator library (used during the reduce function)
-    import time #import the time library; used for determining how quickly the code completed
-    #nltk.download() #this is needed if the nltk books have net yet been downloaded 
-    
-    chunks=4 #how many chunks do we want; equals the number of processes
+def parallel_process(chunks):
+    '''how many chunks do we want; equals the number of processes'''
     partitioned_text = list(chunker(text1.tokens, chunks)) #partition the tokens up into X number of chunks [it's a nested list]
     p = multiprocessing.Pool(chunks) #create a pool of processes equal to the number of chunks
     start_time=time.time() #begin timer
@@ -42,7 +34,22 @@ if __name__ == '__main__':
     p.close() #close the pool of processes
     uppercase_text = reduce(operator.add,uppercase_text) #now reduce the returned chunks (now uppercase) into a single list again
     end_time=time.time() #end the timer
+    print "---%s CHUNKS---" %(chunks)
+    print "\tOriginal: ", text1.tokens[100:105] #see 5 arbitary tokens from the original
+    print "\tUPPERCASE: ", uppercase_text[100:105] #see the same 5 tokens now uppercase
+    print "\ttime: ", end_time-start_time #print the time it took to run the process
     
-    print "Original: ", text1.tokens[100:105] #see 5 arbitary tokens from the original
-    print "UPPERCASE: ", uppercase_text[100:105] #see the same 5 tokens now uppercase
-    print "time: ", end_time-start_time #print the time it took to run the process
+if __name__ == '__main__':
+    '''this is the main function that will set up our pool of processes -- the { if __name ___ '__main__': } is need in order for multiprocessing to work on Windows OS'''
+    #imports were put here because they were loading twice under Windows when put at the top of the file -- this is not an issue in Linux
+    import multiprocessing #import the multiprocessing library
+    import nltk #import the nltk library
+    from nltk.book import text1 #import Moby Dick as an nltk text
+    import operator #import the operator library (used during the reduce function)
+    import time #import the time library; used for determining how quickly the code completed
+    #nltk.download() #this is needed if the nltk books have net yet been downloaded 
+    
+    #RUN THE PROGRAM WITH ONE CHUNK AND ONE PROCESS
+    test_runs = [1,2,4,8]
+    for test in test_runs:
+        parallel_process(test)
